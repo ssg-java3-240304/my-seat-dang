bcd7b3320c86feaee338a03dc4e2fb2743e17bd7
yongjun98 <116868167+yongjun98@users.noreply.github.com>
2024-07-25 00:57:05 +0900

    cl/cd

---
+++ myseatdang/src/main/java/com/sh/myseatdang/MyseatdangApplication.java
@@ -0,0 +1,13 @@
+package com.sh.myseatdang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyseatdangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyseatdangApplication.class, args);
+	}
+
+}
---
+++ myseatdang/src/test/java/com/sh/myseatdang/MyseatdangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.myseatdang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyseatdangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
