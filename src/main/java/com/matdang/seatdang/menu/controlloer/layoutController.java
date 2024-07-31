1555c18e62532d7a67c11d017c51ae003e21cdf2
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-07-31 17:12:49 +0900

    feat(seatdang): create admin & shop layout #19

---
+++ src/main/java/com/matdang/seatdang/menu/controlloer/layoutController.java
@@ -0,0 +1,18 @@
+package com.matdang.seatdang.menu.controlloer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class layoutController {
+    @GetMapping("/admin/main")
+    public void testPage1() {
+
+    }
+
+    @GetMapping("/shop/main")
+    public void testPage2() {
+
+    }
+
+}
