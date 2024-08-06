ae207fc853399bcd8f9234219775565c9df2c434
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-06 14:50:22 +0900

    feat(pay.dto): create PayDetail #108

---
+++ src/main/java/com/matdang/seatdang/pay/dto/PayDetail.java
@@ -0,0 +1,17 @@
+package com.matdang.seatdang.pay.dto;
+
+
+import lombok.*;
+
+@Data
+@Setter(AccessLevel.PRIVATE)
+@Builder
+@AllArgsConstructor
+public class PayDetail {
+    private String partnerOrderId;
+    private String partnerUserId;
+    private String itemName;
+    private Integer quantity;
+    private Integer totalAmount;
+    private Integer taxFreeAmount; // 상품 비과세 금액
+}
