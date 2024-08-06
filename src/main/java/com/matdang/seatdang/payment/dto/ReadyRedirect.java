1988a96ba8eeee470731ab4ca15647e00d6dc3c6
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-06 23:24:29 +0900

    feat(payment): create ReadyRedirect DTO #108 - pay 준비 redirect시, 넘어오는 query parameter ->  DTO mapping

---
+++ src/main/java/com/matdang/seatdang/payment/dto/ReadyRedirect.java
@@ -0,0 +1,11 @@
+package com.matdang.seatdang.payment.dto;
+
+
+import lombok.Data;
+
+@Data
+public class ReadyRedirect {
+    private String pg_token; // kakao api에서 정의된 명칭
+    private String partnerOrderId;
+    private String partnerUserId;
+}
