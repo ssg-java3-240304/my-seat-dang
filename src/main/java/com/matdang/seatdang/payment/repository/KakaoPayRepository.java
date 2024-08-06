38fb954d703bfd7c0a820634adc8e53140a5cbfd
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-06 23:22:21 +0900

    feat(payment): create KakaoPayRepository #108

---
+++ src/main/java/com/matdang/seatdang/payment/repository/KakaoPayRepository.java
@@ -0,0 +1,12 @@
+package com.matdang.seatdang.payment.repository;
+
+import com.matdang.seatdang.payment.entity.PayReady;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface KakaoPayRepository extends JpaRepository<PayReady, String> {
+    @Query("SELECT p.tid FROM PayReady p WHERE p.partnerOrderId = :partnerOrderId AND p.partnerUserId = :partnerUserId")
+    String findTidByPartnerOrderIdAndPartnerUserId(@Param("partnerOrderId") String partnerOrderId,
+                                                   @Param("partnerUserId") String partnerUserId);
+}
