4b5f6be03037ed23d330be040d38c291e11f776f
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-06 23:23:43 +0900

    comment(KakaoPayRepository): write TODO #108

--- src/main/java/com/matdang/seatdang/payment/repository/KakaoPayRepository.java
+++ src/main/java/com/matdang/seatdang/payment/repository/KakaoPayRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+// TODO : Test 필요
 public interface KakaoPayRepository extends JpaRepository<PayReady, String> {
     @Query("SELECT p.tid FROM PayReady p WHERE p.partnerOrderId = :partnerOrderId AND p.partnerUserId = :partnerUserId")
     String findTidByPartnerOrderIdAndPartnerUserId(@Param("partnerOrderId") String partnerOrderId,
