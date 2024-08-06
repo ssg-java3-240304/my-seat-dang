4cce23855d8a8f408522bffb60a1ddbb4aebea8e
nnieun <abocado216@naver.com>
2024-08-06 18:10:38 +0900

    Feat : controller, reopsitory #62

---
+++ src/main/java/com/matdang/seatdang/member/controller/StoreOwnerController.java
@@ -0,0 +1,13 @@
+package com.matdang.seatdang.member.controller;
+
+//점주
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/")
+public class StoreOwnerController {
+}
