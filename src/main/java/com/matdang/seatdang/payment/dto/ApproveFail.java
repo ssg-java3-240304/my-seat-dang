44ede1b2ab28e829a3438b4b07f5471f7599f20f
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-07 17:18:36 +0900

    feat(payment): create approve fail dto #108 - 내부에 Extras 객체 포함

---
+++ src/main/java/com/matdang/seatdang/payment/dto/ApproveFail.java
@@ -0,0 +1,18 @@
+package com.matdang.seatdang.payment.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Setter;
+
+@Data
+@Setter(AccessLevel.PRIVATE)
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class ApproveFail {
+    private Integer errorCode;
+    private String errorMessage;
+    private Extras extras;
+}
