36cbe118d9bd21c5d496378840cb283ffb7daf26
jeongeun1 <162237367+jeongeun1@users.noreply.github.com>
2024-08-07 10:36:34 +0900

    Update: 매장목록 사진 추가 및 resources-static-store 삭제 #27

--- src/main/java/com/matdang/seatdang/store/service/StoreService.java
+++ src/main/java/com/matdang/seatdang/store/service/StoreService.java
@@ -1,5 +1,6 @@
 package com.matdang.seatdang.store.service;
 
+import com.matdang.seatdang.common.storeEnum.StoreType;
 import com.matdang.seatdang.store.dto.StoreListResponseDto;
 import com.matdang.seatdang.store.entity.Store;
 import com.matdang.seatdang.store.repository.StoreRepository;
@@ -9,15 +10,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import static com.matdang.seatdang.common.storeEnum.StoreType.CUSTOM;
+import static com.matdang.seatdang.common.storeEnum.StoreType.GENERAL_RESERVATION;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class StoreService {
     private final StoreRepository storeRepository;
-    public Page<StoreListResponseDto> findAll(String q, Pageable pageable) {
-        Page<Store> storePage = q != null ?
-                storeRepository.findByStoreNameContaining(q, pageable) :
-                storeRepository.findAll(pageable);
+    public Page<StoreListResponseDto> findAll(Pageable pageable) {
+        Page<Store> storePage = storeRepository.findAll(pageable);
+        return storePage.map(StoreListResponseDto::fromStore);
+    }
+
+    public Page<StoreListResponseDto> findByStoreTypeContaining(StoreType storeType, StoreType storeType1, Pageable pageable) {
+        Page<Store> storePage = storeRepository.findByStoreTypeContaining(GENERAL_RESERVATION, CUSTOM, pageable);
         return storePage.map(StoreListResponseDto::fromStore);
     }
 }
