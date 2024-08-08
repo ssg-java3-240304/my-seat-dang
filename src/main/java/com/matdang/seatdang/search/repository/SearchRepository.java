e5d8b0d3024d9c7ba81e55c06bc3f1e72a3ad2fb
CstoneKu <cstoneku@gmail.com>
2024-08-09 01:12:58 +0900

    Feat: 매장명으로 검색 & 지역명으로 검색 테스트 코드 작성 #58 #59

--- src/main/java/com/matdang/seatdang/search/repository/SearchRepository.java
+++
@@ -1,8 +0,0 @@
-package com.matdang.seatdang.search.repository;
-
-import com.matdang.seatdang.store.entity.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface SearchRepository extends JpaRepository<Store, Long> {
-}
