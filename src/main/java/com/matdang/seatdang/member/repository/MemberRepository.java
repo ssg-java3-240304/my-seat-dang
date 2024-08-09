522b02d8dcf0a55d8f7e643cda0f1059751d141a
nnieun <abocado216@naver.com>
2024-08-09 11:04:01 +0900

    Feat: 로그인 기능 추가 #97

--- src/main/java/com/matdang/seatdang/member/repository/MemberRepository.java
+++ src/main/java/com/matdang/seatdang/member/repository/MemberRepository.java
@@ -5,4 +5,5 @@ import com.matdang.seatdang.member.entitiy.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByMemberEmail(String memberEmail);
 }
