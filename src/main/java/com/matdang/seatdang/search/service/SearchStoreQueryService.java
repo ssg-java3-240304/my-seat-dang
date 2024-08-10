302cd8cf427badc95c138aaca5fc812b1f1665d4
CstoneKu <162235645+CstoneKu@users.noreply.github.com>
2024-08-11 00:18:01 +0900

    Feat: SearchStoreQueryService 이름검색 & 지역검색 구현 #59 #58

--- src/main/java/com/matdang/seatdang/search/service/SearchStoreQueryService.java
+++ src/main/java/com/matdang/seatdang/search/service/SearchStoreQueryService.java
@@ -3,20 +3,30 @@ package com.matdang.seatdang.search.service;
 import com.matdang.seatdang.store.dto.StoreListResponseDto;
 import com.matdang.seatdang.store.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class SearchStoreService {
+public class SearchStoreQueryService {
     private final StoreRepository storeRepository;
     /**
      * 상호로 검색
      */
     public Page<StoreListResponseDto> searchStoreByStoreName(String storeName, Pageable pageable) {
-
+        log.debug("searchStoreByStoreName: {}", storeName);
         return storeRepository.findByStoreNameContaining(storeName, pageable)
                 .map((store -> StoreListResponseDto.fromStore(store)));
     }
+
+    public Page<StoreListResponseDto> searchStoreByAddress(String storeAddress, Pageable pageable) {
+        log.debug("searchStoreByAddress: {}", storeAddress);
+        return storeRepository.findByStoreAddressContaining(storeAddress, pageable)
+                .map((store -> StoreListResponseDto.fromStore(store)));
+    }
+
+
 }
