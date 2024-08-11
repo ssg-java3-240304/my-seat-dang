462684cd9c53b234957b190b834fb821b96e60a2
yongjun98 <116868167+yongjun98@users.noreply.github.com>
2024-08-11 17:45:12 +0900

    Feat: 몽고db capped 설정 자동화 test중 #46

--- src/main/java/com/matdang/seatdang/config/ChatConfig.java
+++ src/main/java/com/matdang/seatdang/config/ChatConfig.java
@@ -2,25 +2,43 @@ package com.matdang.seatdang.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.CollectionOptions;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "com.matdang.seatdang.chat.repository")
 public class ChatConfig {
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @Bean
     public MongoClient mongoClient() {
         // MongoDB 클라이언트를 생성하는 부분
-        return MongoClients.create("mongodb://localhost:27017");
+        return MongoClients.create("mongodb://ssg-java3.iptime.org:27017");
     }
 
     @Bean
     public MongoTemplate mongoTemplate(MongoClient mongoClient) {
         // MongoTemplate을 정의하는 부분, MongoDB의 데이터베이스 이름을 명시
         return new MongoTemplate(mongoClient, "chatdb");
+    }
+
+    @Bean
+    public void initializeCollections() {
+        ensureCappedCollectionExists();
+    }
+
+    public void ensureCappedCollectionExists() {
+        if (!mongoTemplate.collectionExists("chat")) {
+            mongoTemplate.createCollection("chat", CollectionOptions.empty()
+                    .capped()
+                    .maxDocuments(5000)
+                    .size(1000000));
+        }
     }
 }
 
