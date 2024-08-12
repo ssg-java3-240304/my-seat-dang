f5991c32cd50e8daf9f4425f5da43d6bd1715710
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-13 02:14:49 +0900

    rename(payment): separate dto package #199 - only service dto를 서비스 계층 패키지로 이동

--- src/main/java/com/matdang/seatdang/payment/service/dto/ReadyRequest.java
+++ src/main/java/com/matdang/seatdang/payment/service/dto/ReadyRequest.java
@@ -1,4 +1,4 @@
-package com.matdang.seatdang.payment.dto;
+package com.matdang.seatdang.payment.service.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
