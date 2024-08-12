151c8e5c1571a1087e933b18f55f6a0394bf0e78
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-13 02:13:42 +0900

    refactor(payment.dto): apply setter accessLevel private #199

--- src/main/java/com/matdang/seatdang/payment/dto/ReadyRedirect.java
+++ src/main/java/com/matdang/seatdang/payment/dto/ReadyRedirect.java
@@ -1,9 +1,12 @@
 package com.matdang.seatdang.payment.dto;
 
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 @Data
+@Setter(AccessLevel.PRIVATE)
 public class ReadyRedirect {
     private String pg_token; // kakao api에서 정의된 명칭
     private String partnerOrderId;
