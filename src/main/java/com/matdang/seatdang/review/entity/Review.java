0e8c1975c893941526e49dae7cc71da050b477e7
yongjun98 <116868167+yongjun98@users.noreply.github.com>
2024-08-12 14:25:46 +0900

    [1대1문의] 일반회원은 점주에게 이미지를 첨부할 수 있습니다. #182

--- src/main/java/com/matdang/seatdang/review/entity/Review.java
+++ src/main/java/com/matdang/seatdang/review/entity/Review.java
@@ -1,71 +1,71 @@
-package com.matdang.seatdang.review.entity;
-
-
-import com.matdang.seatdang.member.entitiy.Member;
-import com.matdang.seatdang.store.entity.Store;
-import jakarta.persistence.*;
-import lombok.*;
-
-import static jakarta.persistence.FetchType.*;
-import static jakarta.persistence.GenerationType.IDENTITY;
-import static java.util.Objects.requireNonNull;
-
-@EqualsAndHashCode(callSuper = true)
-@Entity
-@Table(name = "tbl_review")
-@Data
-@Setter(AccessLevel.PRIVATE)
-@NoArgsConstructor
-@AllArgsConstructor
-public class Review extends ReviewConfig{
-
-    @Id @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "review_id")
-    private Long id;
-
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
-
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "store_id")
-    private Store store;
-
-    @Column(name = "review_rating")
-    private double rate;
-
-    @Column(name = "review_content")
-    private String content;
-
-    @Column(name = "review_image")
-    private String image;
-
-
-//    //== 연관 관계 메서드 ==// -> member 어떤 멤버가 리뷰를 썼는지 리뷰 클래스에 맴버 객체를 합체
-//    public void setMember(Member member){
-//        this.member = member;
-//        member.getReviewList().add(this);
+//package com.matdang.seatdang.review.entity;
+//
+//
+//import com.matdang.seatdang.member.entitiy.Member;
+//import com.matdang.seatdang.store.entity.Store;
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+//import static jakarta.persistence.FetchType.*;
+//import static jakarta.persistence.GenerationType.IDENTITY;
+//import static java.util.Objects.requireNonNull;
+//
+//@EqualsAndHashCode(callSuper = true)
+//@Entity
+//@Table(name = "tbl_review")
+//@Data
+//@Setter(AccessLevel.PRIVATE)
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class Review extends ReviewConfig{
+//
+//    @Id @GeneratedValue(strategy = IDENTITY)
+//    @Column(name = "review_id")
+//    private Long id;
+//
+//    @ManyToOne(fetch = LAZY)
+//    @JoinColumn(name = "member_id")
+//    private Member member;
+//
+//    @ManyToOne(fetch = LAZY)
+//    @JoinColumn(name = "store_id")
+//    private Store store;
+//
+//    @Column(name = "review_rating")
+//    private double rate;
+//
+//    @Column(name = "review_content")
+//    private String content;
+//
+//    @Column(name = "review_image")
+//    private String image;
+//
+//
+////    //== 연관 관계 메서드 ==// -> member 어떤 멤버가 리뷰를 썼는지 리뷰 클래스에 맴버 객체를 합체
+////    public void setMember(Member member){
+////        this.member = member;
+////        member.getReviewList().add(this);
+////    }
+////    // 어떤 스토어에 내 리뷰를 박을건지 설정하는 작업이다.
+////    public void setStore(Store store){
+////        this.store = store;
+////        store.getReviews().add(this);
+////    }
+//
+//    // private 생성자
+//    private Review(Member member, Store store, String content, String image,double rate) {
+//        setMember(member);
+//        setStore(store);
+//        this.rate = rate;
+//        this.image = image;
+//        this.content = content;
 //    }
-//    // 어떤 스토어에 내 리뷰를 박을건지 설정하는 작업이다.
-//    public void setStore(Store store){
-//        this.store = store;
-//        store.getReviews().add(this);
+//
+//    // 리뷰를 생성하는 메서드인데.
+//    public static Review createReivew(Member member, Store store, String content, String image, double rate){
+//        requireNonNull(content);
+//        requireNonNull(image);
+//        requireNonNull(rate);
+//        return new Review(member,store,content,image,rate);
 //    }
-
-    // private 생성자
-    private Review(Member member, Store store, String content, String image,double rate) {
-        setMember(member);
-        setStore(store);
-        this.rate = rate;
-        this.image = image;
-        this.content = content;
-    }
-
-    // 리뷰를 생성하는 메서드인데.
-    public static Review createReivew(Member member, Store store, String content, String image, double rate){
-        requireNonNull(content);
-        requireNonNull(image);
-        requireNonNull(rate);
-        return new Review(member,store,content,image,rate);
-    }
-}
+//}
--- src/main/java/com/matdang/seatdang/review/repository/ReviewRepository.java
+++ src/main/java/com/matdang/seatdang/review/repository/ReviewRepository.java
@@ -1,8 +1,8 @@
-package com.matdang.seatdang.review.repository;
-
-import com.matdang.seatdang.review.entity.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-
-}
+//package com.matdang.seatdang.review.repository;
+//
+//import com.matdang.seatdang.review.entity.Review;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface ReviewRepository extends JpaRepository<Review, Long> {
+//
+//}
