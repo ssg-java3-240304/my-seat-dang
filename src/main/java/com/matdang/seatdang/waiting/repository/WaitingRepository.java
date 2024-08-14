4b5eb8ad64f6e15f72ff76bb28bbad629c4944d3
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-14 18:42:38 +0900

    feat(waiting): create 웨이팅 취소 상태 조회 #211

--- src/main/java/com/matdang/seatdang/waiting/repository/WaitingRepository.java
+++ src/main/java/com/matdang/seatdang/waiting/repository/WaitingRepository.java
@@ -3,6 +3,7 @@ package com.matdang.seatdang.waiting.repository;
 import com.matdang.seatdang.waiting.entity.Waiting;
 import com.matdang.seatdang.waiting.entity.WaitingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +12,10 @@ public interface WaitingRepository extends JpaRepository<Waiting, Long> {
     List<Waiting> findAllByStoreId(Long storeId);
 
     List<Waiting> findAllByStoreIdAndWaitingStatus(Long storeId, WaitingStatus waitingStatus);
+
+    @Query("select w from Waiting w where w.storeId = :storeId and w.waitingStatus in (" +
+            "com.matdang.seatdang.waiting.entity.WaitingStatus.SHOP_CANCELED, " +
+            "com.matdang.seatdang.waiting.entity.WaitingStatus.NO_SHOW, " +
+            "com.matdang.seatdang.waiting.entity.WaitingStatus.CUSTOMER_CANCELED)")
+    List<Waiting> findAllByCancelStatus(Long storeId);
 }
