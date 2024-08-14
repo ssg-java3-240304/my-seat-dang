690aaed5182f48f05e8d30a86cb125ab113867e4
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-14 18:44:11 +0900

    test(repository): create 상점 id로 취소 상태(노쇼, 점주, 고객) 웨이팅 모두 조회 #211

--- src/test/java/com/matdang/seatdang/waiting/repository/WaitingRepositoryTest.java
+++ src/test/java/com/matdang/seatdang/waiting/repository/WaitingRepositoryTest.java
@@ -1,20 +1,22 @@
 package com.matdang.seatdang.waiting.repository;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.matdang.seatdang.waiting.entity.CustomerInfo;
 import com.matdang.seatdang.waiting.entity.Waiting;
 import com.matdang.seatdang.waiting.entity.WaitingStatus;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.time.LocalDateTime;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -27,18 +29,25 @@ class WaitingRepositoryTest {
 
     @BeforeEach
     void setUp()  {
+        {
+            long i = 0;
+            for (WaitingStatus value : WaitingStatus.values()) {
+                for (int j = 0; j < 10; j++, i++) {
+                    waitingRepository.save(Waiting.builder()
+                            .waitingId(i)
+                            .waitingNumber(i)
+                            .storeId(1L)
+                            .customerInfo(new CustomerInfo(i, "010-1111-1111"))
+                            .waitingStatus(value)
+                            .createdAt(LocalDateTime.now())
+                            .visitedTime(null)
+                            .build());
+
+                }
+            }
+        }
 
         for (long i = 0; i < 10; i++) {
-            waitingRepository.save(Waiting.builder()
-                    .waitingId(i)
-                    .waitingNumber(i)
-                    .storeId(1L)
-                    .customerInfo(new CustomerInfo(i, "010-1111-1111"))
-                    .waitingStatus(WaitingStatus.WAITING)
-                    .createdAt(LocalDateTime.now())
-                    .visitedTime(null)
-                    .build());
-
             waitingRepository.save(Waiting.builder()
                     .waitingId(i)
                     .waitingNumber(i)
@@ -50,33 +59,12 @@ class WaitingRepositoryTest {
                     .build());
         }
 
-        for (long i = 10; i < 20; i++) {
-            waitingRepository.save(Waiting.builder()
-                    .waitingId(i)
-                    .waitingNumber(i)
-                    .storeId(1L)
-                    .customerInfo(new CustomerInfo(i, "010-1111-1111"))
-                    .waitingStatus(WaitingStatus.VISITED)
-                    .createdAt(LocalDateTime.now())
-                    .visitedTime(null)
-                    .build());
-
-            waitingRepository.save(Waiting.builder()
-                    .waitingId(i)
-                    .waitingNumber(i)
-                    .storeId(1L)
-                    .customerInfo(new CustomerInfo(i, "010-1111-1111"))
-                    .waitingStatus(WaitingStatus.NO_SHOW)
-                    .createdAt(LocalDateTime.now())
-                    .visitedTime(null)
-                    .build());
-        }
         em.flush();
-
+        em.clear();
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1,30","2,10"})
+    @CsvSource(value = {"1,50","2,10"})
     @DisplayName("상점 id로 웨이팅 전체 조회")
     void findAllByStoreId(long shopId, int size) {
         // given
@@ -95,6 +83,17 @@ class WaitingRepositoryTest {
         // when
         List<Waiting> findWaitings = waitingRepository.findAllByStoreIdAndWaitingStatus(shopId, WaitingStatus.valueOf(status));
 
+        // then
+        assertThat(findWaitings.size()).isEqualTo(size);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,30","2,0"})
+    @DisplayName("상점 id로 취소 상태(노쇼, 점주, 고객) 웨이팅 모두 조회")
+    void findAllByCancelStatus(Long storeId, int size) {
+        // given
+        // when
+        List<Waiting> findWaitings = waitingRepository.findAllByCancelStatus(storeId);
         // then
         assertThat(findWaitings.size()).isEqualTo(size);
     }
