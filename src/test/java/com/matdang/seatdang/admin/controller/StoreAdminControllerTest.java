e17e64f744f170c20d06cb7f0e7efb209264b64f
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-14 16:32:54 +0900

    fix(test): fix typing error

--- src/test/java/com/matdang/seatdang/admin/controller/StoreAdminControllerTest.java
+++ src/test/java/com/matdang/seatdang/admin/controller/StoreAdminControllerTest.java
@@ -1,17 +1,10 @@
 package com.matdang.seatdang.admin.controller;
 
-import com.matdang.seatdang.admin.dto.StoreDetailReponseDto;
 import com.matdang.seatdang.admin.repository.StoreAdminRepository;
-import com.matdang.seatdang.admin.service.StoreAdminService;
-import com.matdang.seatdang.store.entity.Store;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -31,4 +24,3 @@ class StoreAdminControllerTest {
 
 
 
-}
