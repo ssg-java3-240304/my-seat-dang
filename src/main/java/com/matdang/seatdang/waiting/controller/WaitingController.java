688923b8188c13735c40d7537ee9466ff8929e29
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-16 00:48:46 +0900

    feat(waiting.controller): create 웨이팅 입장 & 취소 #219

--- src/main/java/com/matdang/seatdang/waiting/controller/WaitingController.java
+++ src/main/java/com/matdang/seatdang/waiting/controller/WaitingController.java
@@ -1,5 +1,6 @@
 package com.matdang.seatdang.waiting.controller;
 
+import com.matdang.seatdang.waiting.dto.UpdateRequest;
 import com.matdang.seatdang.waiting.entity.CustomerInfo;
 import com.matdang.seatdang.waiting.entity.Waiting;
 import com.matdang.seatdang.waiting.entity.WaitingStatus;
@@ -11,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -26,9 +29,20 @@ public class WaitingController {
     private final WaitingService waitingService;
 
     @GetMapping
-    public String showWaiting(@RequestParam(defaultValue = "0") int status, Model model) {
+    public String showWaiting(@RequestParam(defaultValue = "0") int status,
+                              Model model) {
         List<Waiting> waitings =waitingService.showWaiting(1L, status);
         model.addAttribute("waitings", waitings);
+        model.addAttribute("status", status);
+        return "store/waiting/main";
+    }
+
+    @PostMapping
+    public String updateStatus(@ModelAttribute UpdateRequest updateRequest, Model model) {
+        waitingService.updateStatus(updateRequest);
+        List<Waiting> waitings =waitingService.showWaiting(1L, updateRequest.getStatus());
+        model.addAttribute("waitings", waitings);
+        model.addAttribute("status", updateRequest.getStatus());
         return "store/waiting/main";
     }
 
@@ -38,24 +52,26 @@ public class WaitingController {
     @PostConstruct
     public void initData() {
         {
-            long i = 0;
+            long i = 1;
             for (WaitingStatus value : WaitingStatus.values()) {
-                for (int j = 0; j < 10; j++, i++) {
-                    waitingRepository.save(Waiting.builder()
-                            .waitingId(i)
-                            .waitingNumber(i)
-                            .storeId(1L)
-                            .customerInfo(new CustomerInfo(i, "010-1111-1111"))
-                            .waitingStatus(value)
-                            .createdAt(LocalDateTime.now())
-                            .visitedTime(null)
-                            .build());
+                if (value != WaitingStatus.VISITED) {
+                    for (int j = 0; j < 10; j++, i++) {
+                        waitingRepository.save(Waiting.builder()
+                                .waitingId(i)
+                                .waitingNumber(i)
+                                .storeId(1L)
+                                .customerInfo(new CustomerInfo(i, "010-1111-1111"))
+                                .waitingStatus(value)
+                                .createdAt(LocalDateTime.now())
+                                .visitedTime(null)
+                                .build());
 
+                    }
                 }
             }
         }
 
-        for (long i = 0; i < 10; i++) {
+        for (long i = 1; i <= 10; i++) {
             waitingRepository.save(Waiting.builder()
                     .waitingId(i)
                     .waitingNumber(i)
