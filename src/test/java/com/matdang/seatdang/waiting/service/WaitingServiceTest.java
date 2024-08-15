83e414d3263bc993addb34df56ce0acdb8e93e30
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-16 00:47:28 +0900

    test(waiting): create 웨이팅 상태 업데이트 #219

--- src/test/java/com/matdang/seatdang/waiting/service/WaitingServiceTest.java
+++ src/test/java/com/matdang/seatdang/waiting/service/WaitingServiceTest.java
@@ -1,5 +1,6 @@
 package com.matdang.seatdang.waiting.service;
 
+import com.matdang.seatdang.waiting.dto.UpdateRequest;
 import com.matdang.seatdang.waiting.entity.CustomerInfo;
 import com.matdang.seatdang.waiting.entity.Waiting;
 import com.matdang.seatdang.waiting.entity.WaitingStatus;
@@ -7,6 +8,7 @@ import com.matdang.seatdang.waiting.repository.WaitingRepository;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -74,6 +76,28 @@ class WaitingServiceTest {
         List<Waiting> waitings = waitingService.showWaiting(storeId, status);
         // then
         assertThat(waitings.size()).isEqualTo(size);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,1,VISITED", "2,1,SHOP_CANCELED"})
+    @DisplayName("웨이팅 상태 업데이트")
+    void updateStatus(int status, Long shopId, WaitingStatus waitingStatus) {
+        // given
+        List<Waiting> findWaitings = waitingRepository.findAll();
+        em.clear();
+
+        UpdateRequest updateRequest = new UpdateRequest();
+        updateRequest.setChangeStatus(status);
+        updateRequest.setStoreId(shopId);
+        updateRequest.setId(findWaitings.get(0).getId());
+        updateRequest.setWaitingNumber(1L);
+
+        // when
+        waitingService.updateStatus(updateRequest);
+
+        // then
+        assertThat(waitingRepository.findById(findWaitings.get(0).getId()).get().getWaitingStatus())
+                .isEqualTo(waitingStatus);
     }
 
 }
