d802fd77c2598b25ec4056ce6361e588b9bc53a0
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-16 00:46:08 +0900

    feat(waiting.service): create updateStatus() #219 - add 웨이팅 입장 or 취소로 업데이트

--- src/main/java/com/matdang/seatdang/waiting/service/WaitingService.java
+++ src/main/java/com/matdang/seatdang/waiting/service/WaitingService.java
@@ -1,5 +1,6 @@
 package com.matdang.seatdang.waiting.service;
 
+import com.matdang.seatdang.waiting.dto.UpdateRequest;
 import com.matdang.seatdang.waiting.entity.Waiting;
 import com.matdang.seatdang.waiting.entity.WaitingStatus;
 import com.matdang.seatdang.waiting.repository.WaitingRepository;
@@ -10,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class WaitingService {
     private final WaitingRepository waitingRepository;
@@ -20,8 +21,26 @@ public class WaitingService {
             return waitingRepository.findAllByCancelStatus(storeId);
         }
 
-        List<Waiting> waitings = waitingRepository.findAllByStoreIdAndWaitingStatus(storeId, WaitingStatus.findWaiting(status));
+        List<Waiting> waitings = waitingRepository.findAllByStoreIdAndWaitingStatus(storeId,
+                WaitingStatus.findWaiting(status));
         return waitings;
+    }
+
+    @Transactional
+    public void updateStatus(UpdateRequest updateRequest) {
+        if (updateRequest.getChangeStatus() != null) {
+            if (updateRequest.getChangeStatus() == 1) {
+                waitingRepository.updateAllWaitingNumberByVisit(updateRequest.getStoreId());
+            }
+            if (updateRequest.getChangeStatus() == 2) {
+                waitingRepository.updateWaitingNumberByCancel(updateRequest.getStoreId(),
+                        updateRequest.getWaitingNumber());
+            }
+
+            waitingRepository.updateStatus(WaitingStatus.findWaiting(updateRequest.getChangeStatus()),
+                    updateRequest.getId());
+        }
+
     }
 
 
