e84f7e8099db36e08c8cf349119fb2ae215fa967
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-18 02:31:20 +0900

    test(store): create 웨이팅 상태 가져오기 #234

--- src/test/java/com/matdang/seatdang/store/repository/query/dto/StoreQueryRepositoryTest.java
+++ src/test/java/com/matdang/seatdang/store/repository/query/dto/StoreQueryRepositoryTest.java
@@ -5,10 +5,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.matdang.seatdang.store.entity.Store;
 import com.matdang.seatdang.store.repository.StoreRepository;
 import com.matdang.seatdang.store.vo.StoreSetting;
+import com.matdang.seatdang.store.vo.WaitingStatus;
 import com.matdang.seatdang.store.vo.WaitingTime;
+import jakarta.persistence.EntityManager;
 import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +24,8 @@ class StoreQueryRepositoryTest {
     private StoreRepository storeRepository;
     @Autowired
     private StoreQueryRepository storeQueryRepository;
+    @Autowired
+    private EntityManager em;
 
     @Test
     @DisplayName("이용가능한 웨이팅 시간 가져오기")
@@ -33,6 +39,8 @@ class StoreQueryRepositoryTest {
                                 .build())
                         .build())
                 .build());
+        em.flush();
+        em.clear();
         // when
 
         AvailableWaitingTime findResult = storeQueryRepository.findAvailableWaitingTime(store.getStoreId());
@@ -54,6 +62,8 @@ class StoreQueryRepositoryTest {
                                 .build())
                         .build())
                 .build());
+        em.flush();
+        em.clear();
         // when
 
         LocalTime findResult = storeQueryRepository.findEstimatedWaitingTime(store.getStoreId());
@@ -63,4 +73,24 @@ class StoreQueryRepositoryTest {
         assertThat(findResult).isEqualTo(LocalTime.of(0, 20));
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"OPEN", "CLOSE","UNAVAILABLE"})
+    @DisplayName("웨이팅 상태 가져오기")
+    public void findWaitingStatus(String waitingStatus) {
+        // given
+        Store store = storeRepository.save(Store.builder()
+                .storeSetting(StoreSetting.builder()
+                        .waitingStatus(WaitingStatus.valueOf(waitingStatus))
+                        .build())
+                .build());
+        em.flush();
+        em.clear();
+        // when
+
+        WaitingStatus findResult = storeQueryRepository.findWaitingStatus(store.getStoreId());
+
+        // then
+        assertThat(findResult).isNotNull();
+        assertThat(findResult).isEqualTo(WaitingStatus.valueOf(waitingStatus));
+    }
 }
