ff67fd2ea6b4a402bb60c28b53177763ff0532e6
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-18 02:46:55 +0900

    feat(WaitingStatus): add field in Enum #234

--- src/main/java/com/matdang/seatdang/store/vo/WaitingStatus.java
+++ src/main/java/com/matdang/seatdang/store/vo/WaitingStatus.java
@@ -1,7 +1,16 @@
 package com.matdang.seatdang.store.vo;
 
+import lombok.Getter;
+
+@Getter
 public enum WaitingStatus {
-    OPEN,
-    CLOSE,
-    UNAVAILABLE
+    OPEN("웨이팅 접수"),
+    CLOSE("웨이팅 마감"),
+    UNAVAILABLE("웨이팅 이용불가");
+
+    private final String waitingStatus;
+
+    WaitingStatus(String waitingStatus) {
+        this.waitingStatus = waitingStatus;
+    }
 }
