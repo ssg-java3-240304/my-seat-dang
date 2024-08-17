be4cd20435760d7388bbeb02a55fbc0d014d545b
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-17 20:00:51 +0900

    feat(store.vo): modify WaitingTime #231 - add @Builder

--- src/main/java/com/matdang/seatdang/store/vo/WaitingTime.java
+++ src/main/java/com/matdang/seatdang/store/vo/WaitingTime.java
@@ -3,12 +3,16 @@ package com.matdang.seatdang.store.vo;
 import jakarta.persistence.Embeddable;
 import java.time.LocalTime;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Embeddable
 @Data
+@Builder
+@AllArgsConstructor
 @Setter(AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WaitingTime {
