97f43b322884377bbdfb60df35df93992b132cdc
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-17 23:54:20 +0900

    test(store): create 상점 웨이팅 상태 업데이트 #234

--- src/test/java/com/matdang/seatdang/store/repository/StoreRepositoryTest.java
+++ src/test/java/com/matdang/seatdang/store/repository/StoreRepositoryTest.java
@@ -4,10 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.matdang.seatdang.store.entity.Store;
+import com.matdang.seatdang.store.vo.Status;
 import jakarta.persistence.EntityManager;
 import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -58,5 +61,23 @@ class StoreRepositoryTest {
         assertThat(result).isEqualTo(1);
         assertThat(storeRepository.findByStoreId(1L).getStoreSetting().getWaitingTime()
                 .getEstimatedWaitingTime()).isEqualTo(LocalTime.of(0, 20));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ON", "OFF"})
+    @DisplayName("상점 웨이팅 상태 업데이트")
+    void updateWaitingStatus(String status) {
+        // given
+        storeRepository.save(Store.builder()
+                .storeId(1L)
+                .build());
+        em.clear();
+
+        // when
+        int result = storeRepository.updateWaitingStatus(Status.valueOf(status), 1L);
+
+        // then
+        assertThat(result).isEqualTo(1);
+        assertThat(storeRepository.findByStoreId(1L).getStoreSetting().getWaitingStatus()).isEqualTo(Status.valueOf(status));
     }
 }
