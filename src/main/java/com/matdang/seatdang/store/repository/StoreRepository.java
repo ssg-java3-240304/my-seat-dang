c3f4f7aa3eb8b56d4bd1f5f092306b9e1cb2fbeb
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-17 19:23:44 +0900

    feat(StoreRepository): create 웨이팅 이용가능 시간 업데이트 #231

--- src/main/java/com/matdang/seatdang/store/repository/StoreRepository.java
+++ src/main/java/com/matdang/seatdang/store/repository/StoreRepository.java
@@ -2,21 +2,29 @@ package com.matdang.seatdang.store.repository;
 
 import com.matdang.seatdang.common.storeEnum.StoreType;
 import com.matdang.seatdang.store.entity.Store;
+import java.time.LocalTime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
+    Page<Store> findByStoreNameContainingAndStoreAddressContainingOrderByStoreAddressDesc(String storeName,
+                                                                                          String storeAddress,
+                                                                                          Pageable pageable);
 
-
-    Page<Store> findByStoreNameContainingAndStoreAddressContainingOrderByStoreAddressDesc(String storeName, String storeAddress, Pageable pageable);
     Page<Store> findByStoreNameContainingOrderByStoreAddressDesc(String storeAddress, Pageable pageable);
+
     Page<Store> findByStoreAddressContainingOrderByStoreAddressDesc(String storeAddress, Pageable pageable);
+
     Page<Store> findByStoreNameContainingOrderByStoreAddressAsc(String storeAddress, Pageable pageable);
+
     Page<Store> findByStoreAddressContainingOrderByStoreAddressAsc(String storeAddress, Pageable pageable);
+
     Page<Store> findByStoreAddressContaining(String storeAddress, Pageable pageable);
+
     Page<Store> findByStoreNameContaining(String storeName, Pageable pageable);
 
     Page<Store> findAllByOrderByStarRatingDesc(Pageable pageable);
@@ -24,4 +32,16 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
     Page<Store> findByStoreTypeContaining(StoreType storeType, StoreType storeType1, Pageable pageable);
 
     Store findByStoreId(Long storeId);
+
+    @Modifying
+    @Query("update Store s"
+            + " set s.storeSetting.waitingTime.waitingOpenTime = :waitingOpenTime,"
+            + " s.storeSetting.waitingTime.waitingCloseTime = :waitingCloseTime"
+            + " where s.storeId = :storeId")
+    int updateWaitingAvailableTime(@Param("waitingOpenTime") LocalTime waitingOpenTime,
+                                   @Param("waitingCloseTime") LocalTime waitingCloseTime,
+                                   @Param("storeId") Long storeId);
+
+
+
 }
