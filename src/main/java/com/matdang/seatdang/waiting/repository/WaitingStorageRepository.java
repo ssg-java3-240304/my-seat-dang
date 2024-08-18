4af5b9442c38f9a68e8eeda80734775d620ed321
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-18 19:29:58 +0900

    feat(waiting): create WaitingStorageRepository #240

---
+++ src/main/java/com/matdang/seatdang/waiting/repository/WaitingStorageRepository.java
@@ -0,0 +1,9 @@
+package com.matdang.seatdang.waiting.repository;
+
+import com.matdang.seatdang.waiting.entity.WaitingStorage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WaitingStorageRepository extends JpaRepository<WaitingStorage, Long> {
+
+
+}
