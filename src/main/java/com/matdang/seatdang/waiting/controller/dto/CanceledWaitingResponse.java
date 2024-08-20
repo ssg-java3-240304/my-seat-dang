ad1524835f28cc85691dc822746204be7875d940
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-20 22:40:25 +0900

    feat(waiting): create CanceledWaitingResponse dto #283

---
+++ src/main/java/com/matdang/seatdang/waiting/controller/dto/CanceledWaitingResponse.java
@@ -0,0 +1,32 @@
+package com.matdang.seatdang.waiting.controller.dto;
+
+import com.matdang.seatdang.store.entity.Store;
+import com.matdang.seatdang.waiting.entity.Waiting;
+import com.matdang.seatdang.waiting.entity.WaitingStatus;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CanceledWaitingResponse {
+    private Long waitingNumber;
+    private WaitingStatus waitingStatus;
+    private LocalDateTime createdDate;
+    private Integer peopleCount;
+    private String storeName;
+
+    public static CanceledWaitingResponse create(Waiting waiting, Store store) {
+        CanceledWaitingResponse canceledWaitingResponse = new CanceledWaitingResponse();
+        canceledWaitingResponse.waitingNumber = waiting.getWaitingNumber();
+        canceledWaitingResponse.waitingStatus = waiting.getWaitingStatus();
+        canceledWaitingResponse.createdDate = waiting.getCreatedDate();
+        canceledWaitingResponse.peopleCount = waiting.getCustomerInfo().getPeopleCount();
+        canceledWaitingResponse.storeName = store.getStoreName();
+
+        return canceledWaitingResponse;
+    }
+}
