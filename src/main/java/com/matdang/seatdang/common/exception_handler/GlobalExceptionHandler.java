2f094b42ec43b1ae8e6c4a357503b5cbbf63cb18
CstoneKu <162235645+CstoneKu@users.noreply.github.com>
2024-08-20 12:25:00 +0900

    Feat: page not found 404 페이지 연결 #279

--- src/main/java/com/matdang/seatdang/common/exception_handler/GlobalExceptionHandler.java
+++ src/main/java/com/matdang/seatdang/common/exception_handler/GlobalExceptionHandler.java
@@ -6,7 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 @Slf4j
@@ -27,9 +29,19 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(ReservationException.class)
-    public String handleReservationException(ReservationException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+    public String handleReservationException(ReservationException e, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        log.info("@ControllerAdvice#ReservationException");
+        log.error(e.getMessage(), e); // 예외로깅
         String referer = request.getHeader("Referer");  // 이전 페이지의 URL 가져오기
-        redirectAttributes.addFlashAttribute("errorMessage", ex.getMessage());
+        redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
         return "redirect:" + referer;  // 이전 페이지로 리다이렉트
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public String handleNotFound(NoResourceFoundException e, Model model) {
+        log.info("@ControllerAdvice#handleNotFound");
+        log.error(e.getMessage(), e); // 예외로깅
+        model.addAttribute("errorMessage", "요청하신 페이지를 찾을 수 없습니다.");
+        return "customer/error/404";  // 404 에러 페이지로 이동
     }
 }
