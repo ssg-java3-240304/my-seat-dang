3ff36a75b61078541451be54197844e7d4c5b361
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-21 15:39:58 +0900

    feat(waiting): create 웨이팅을 등록한 상태에서 다시 웨이팅 불가 #290

--- src/main/java/com/matdang/seatdang/waiting/controller/WaitingCustomerController.java
+++ src/main/java/com/matdang/seatdang/waiting/controller/WaitingCustomerController.java
@@ -1,5 +1,6 @@
 package com.matdang.seatdang.waiting.controller;
 
+import com.matdang.seatdang.auth.service.AuthService;
 import com.matdang.seatdang.store.entity.Store;
 import com.matdang.seatdang.store.repository.StoreRepository;
 import com.matdang.seatdang.waiting.controller.dto.AwaitingWaitingResponse;
@@ -10,6 +11,7 @@ import com.matdang.seatdang.waiting.controller.dto.WaitingRequest;
 import com.matdang.seatdang.waiting.entity.Waiting;
 import com.matdang.seatdang.waiting.repository.WaitingRepository;
 import com.matdang.seatdang.waiting.service.WaitingCustomerService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,15 +28,28 @@ public class WaitingCustomerController {
     private final WaitingCustomerService waitingCustomerService;
     private final WaitingRepository waitingRepository;
     private final StoreRepository storeRepository;
+    private final AuthService authService;
+
+    @GetMapping("/test-store")
+    public String showStore(@RequestParam(defaultValue = "1") Long storeId, Model model) {
+        Long memberId = authService.getAuthenticatedMember().getMemberId();
+        boolean isRegistered = waitingRepository.isRegisteredWaiting(storeId, memberId);
+
+        model.addAttribute("storeId", storeId);
+        model.addAttribute("isRegistered", isRegistered);
+
+        return "customer/waiting/test-store";
+    }
 
     /**
      * TODO : 삭제 필요
      * defaultValue는 test 용도임
      */
     // TODO : 웨이팅 후 다시 주소 접근 차단
-    @GetMapping("/waiting")
-    public String readyWaiting(@RequestParam(defaultValue = "1") Long storeId, Model model) {
+    @GetMapping("/waiting/{storeId}")
+    public String readyWaiting(@PathVariable Long storeId, Model model) {
         Store store = storeRepository.findByStoreId(storeId);
+
         model.addAttribute("waitingTeam", waitingRepository.countWaitingByStoreIdAndWaitingStatus(storeId));
         model.addAttribute("readyWaitingResponse", ReadyWaitingResponse.create(store));
 
