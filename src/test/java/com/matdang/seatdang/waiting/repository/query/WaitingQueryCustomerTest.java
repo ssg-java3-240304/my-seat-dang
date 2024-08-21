11af51771479e548e33b5e715ee78488f0bcd131
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-21 17:35:52 +0900

    test(waiting): create 고객 id로 취소상태의 웨이팅들 가져오기 #291

--- src/test/java/com/matdang/seatdang/waiting/repository/query/WaitingQueryCustomerTest.java
+++ src/test/java/com/matdang/seatdang/waiting/repository/query/WaitingQueryCustomerTest.java
@@ -12,6 +12,7 @@ import com.matdang.seatdang.waiting.repository.query.dto.WaitingInfoDto;
 import jakarta.persistence.EntityManager;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -61,12 +62,43 @@ public class WaitingQueryCustomerTest {
         // when
         List<WaitingInfoDto> findResult = waitingQueryRepository.findAllByCustomerIdAndWaitingStatus(
                 1L, WaitingStatus.valueOf(status));
-        System.out.println("findResult = " + findResult);
 
         // then
         assertThat(findResult.size()).isEqualTo(1);
         assertThat(findResult).extracting(WaitingInfoDto::getWaitingStatus)
                 .containsExactly(WaitingStatus.valueOf(status));
         assertThat(findResult).extracting(WaitingInfoDto::getStoreName).containsExactly("마싯당");
+    }
+
+    @Test
+    @DisplayName("고객 id로 취소상태의 웨이팅들 가져오기")
+    void findAllByCustomerIdAndCancelStatus() {
+        // given
+        Store storeA = storeRepository.save(Store.builder()
+                .storeName("마싯당")
+                .build());
+
+        for (WaitingStatus status : WaitingStatus.values()) {
+            waitingRepository.save(Waiting.builder()
+                    .waitingNumber(1L)
+                    .waitingOrder(1L)
+                    .storeId(storeA.getStoreId())
+                    .customerInfo(new CustomerInfo(1L, "010-1111-1111", ((int) (Math.random() * 3 + 1))))
+                    .waitingStatus(status)
+                    .visitedTime(null)
+                    .build());
+        }
+        em.flush();
+        em.clear();
+
+        // when
+        List<WaitingInfoDto> findResult = waitingQueryRepository.findAllByCustomerIdAndCancelStatus(
+                1L);
+
+        // then
+        assertThat(findResult.size()).isEqualTo(3);
+        assertThat(findResult).extracting(WaitingInfoDto::getWaitingStatus)
+                .containsExactlyInAnyOrder(WaitingStatus.NO_SHOW, WaitingStatus.CUSTOMER_CANCELED, WaitingStatus.SHOP_CANCELED);
+        assertThat(findResult).extracting(WaitingInfoDto::getStoreName).contains("마싯당");
     }
 }
