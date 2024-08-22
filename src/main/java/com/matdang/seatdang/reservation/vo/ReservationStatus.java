433e4fb857c6af28181f26a9c5eeef89b33d0758
CstoneKu <162235645+CstoneKu@users.noreply.github.com>
2024-08-22 09:53:03 +0900

    feat(reservation): 예약 상태 변경 검사 기능 #302

--- src/main/java/com/matdang/seatdang/reservation/vo/ReservationStatus.java
+++ src/main/java/com/matdang/seatdang/reservation/vo/ReservationStatus.java
@@ -1,5 +1,10 @@
 package com.matdang.seatdang.reservation.vo;
 
 public enum ReservationStatus {
-    CREATING, DETAILING, AWAITING_PAYMENT, PAYMENT_COMPLETED, CANCELED_BY_OWNER, CANCELED_BY_CUSTOMER, BEFORE_VISIT, VISIT_COMPLETED
+    CREATING, DETAILING, AWAITING_PAYMENT, PAYMENT_COMPLETED, BEFORE_VISIT, VISIT_COMPLETED, CANCELED_BY_OWNER, CANCELED_BY_CUSTOMER;
+
+    public boolean canTransitionTo(ReservationStatus newStatus) {
+        // 현재 상태의 순서가 전환하려는 상태의 순서보다 작아야만 전환 가능
+        return this.ordinal() < newStatus.ordinal();
+    }
 }
