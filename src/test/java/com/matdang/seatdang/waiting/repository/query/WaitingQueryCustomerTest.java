37b9e5c1f03d61fc4a05ee3a0313e7cdc89bfea3
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-22 13:20:11 +0900

    test(waiting): create 고객 id로 특정 상태의 웨이팅(기존 + 저장소) 모두 가져오기 #298 #300

--- src/test/java/com/matdang/seatdang/waiting/repository/query/WaitingQueryCustomerTest.java
+++ src/test/java/com/matdang/seatdang/waiting/repository/query/WaitingQueryCustomerTest.java
@@ -7,8 +7,11 @@ import com.matdang.seatdang.store.repository.StoreRepository;
 import com.matdang.seatdang.waiting.entity.CustomerInfo;
 import com.matdang.seatdang.waiting.entity.Waiting;
 import com.matdang.seatdang.waiting.entity.WaitingStatus;
+import com.matdang.seatdang.waiting.entity.WaitingStorage;
 import com.matdang.seatdang.waiting.repository.WaitingRepository;
+import com.matdang.seatdang.waiting.repository.WaitingStorageRepository;
 import com.matdang.seatdang.waiting.repository.query.dto.WaitingInfoDto;
+import com.matdang.seatdang.waiting.repository.query.dto.WaitingInfoProjection;
 import jakarta.persistence.EntityManager;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -27,6 +30,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class WaitingQueryCustomerTest {
     @Autowired
     private WaitingQueryRepository waitingQueryRepository;
+    @Autowired
+    private WaitingStorageRepository waitingStorageRepository;
     @Autowired
     private StoreRepository storeRepository;
     @Autowired
@@ -108,5 +113,63 @@ public class WaitingQueryCustomerTest {
         assertThat(findResult.getContent()).extracting(WaitingInfoDto::getWaitingStatus)
                 .containsExactlyInAnyOrder(WaitingStatus.NO_SHOW, WaitingStatus.CUSTOMER_CANCELED, WaitingStatus.SHOP_CANCELED);
         assertThat(findResult.getContent()).extracting(WaitingInfoDto::getStoreName).contains("마싯당");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"WAITING", "VISITED", "CUSTOMER_CANCELED"})
+    @DisplayName("고객 id로 특정 상태의 웨이팅(기존 + 저장소) 모두 가져오기")
+    void findUnionAllByCustomerIdAndWaitingStatus(String status) {
+        // given
+        Store storeA = storeRepository.save(Store.builder()
+                .storeName("마싯당")
+                .build());
+
+        Waiting waiting = waitingRepository.save(Waiting.builder()
+                .waitingNumber(1L)
+                .waitingOrder(1L)
+                .storeId(storeA.getStoreId())
+                .customerInfo(new CustomerInfo(1L, "010-1111-1111", ((int) (Math.random() * 3 + 1))))
+                .waitingStatus(WaitingStatus.valueOf(status))
+                .visitedTime(null)
+                .build());
+        Waiting waiting2 = waitingRepository.save(Waiting.builder()
+                .waitingNumber(1L)
+                .waitingOrder(1L)
+                .storeId(storeA.getStoreId())
+                .customerInfo(new CustomerInfo(1L, "010-1111-1111", ((int) (Math.random() * 3 + 1))))
+                .waitingStatus(WaitingStatus.SHOP_CANCELED)
+                .visitedTime(null)
+                .build());
+
+        WaitingStorage waiting3 = waitingStorageRepository.save(WaitingStorage.builder()
+                .waitingNumber(1L)
+                .waitingOrder(1L)
+                .storeId(storeA.getStoreId())
+                .customerInfo(new CustomerInfo(1L, "010-1111-1111", ((int) (Math.random() * 3 + 1))))
+                .waitingStatus(WaitingStatus.valueOf(status))
+                .visitedTime(null)
+                .build());
+        WaitingStorage waiting4 = waitingStorageRepository.save(WaitingStorage.builder()
+                .waitingNumber(1L)
+                .waitingOrder(1L)
+                .storeId(storeA.getStoreId())
+                .customerInfo(new CustomerInfo(1L, "010-1111-1111", ((int) (Math.random() * 3 + 1))))
+                .waitingStatus(WaitingStatus.SHOP_CANCELED)
+                .visitedTime(null)
+                .build());
+        em.flush();
+        em.clear();
+
+        PageRequest pageable = PageRequest.of(0, 10);
+
+        // when
+        Page<WaitingInfoProjection> findResult = waitingQueryRepository.findUnionAllByCustomerIdAndWaitingStatus(
+                1L, WaitingStatus.valueOf(status), pageable);
+
+        // then
+        assertThat(findResult.getTotalElements()).isEqualTo(2);
+        assertThat(findResult.getContent()).extracting(WaitingInfoProjection::getWaitingStatus)
+                .containsOnly(WaitingStatus.valueOf(status));
+        assertThat(findResult.getContent()).extracting(WaitingInfoProjection::getStoreName).containsOnly("마싯당");
     }
 }
