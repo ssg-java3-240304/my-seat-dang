ad1f589562e10fa44c0881261cf62e5d3b0e82ec
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-22 23:05:44 +0900

    test(waiting): 동시에 웨이팅 100개 등록 동시성 테스트 #308

--- src/test/java/com/matdang/seatdang/waiting/service/WaitingCustomerServiceTest.java
+++ src/test/java/com/matdang/seatdang/waiting/service/WaitingCustomerServiceTest.java
@@ -14,7 +14,12 @@ import com.matdang.seatdang.waiting.repository.WaitingStorageRepository;
 import com.matdang.seatdang.waiting.repository.query.dto.WaitingInfoDto;
 import com.matdang.seatdang.waiting.repository.query.dto.WaitingInfoProjection;
 import jakarta.persistence.EntityManager;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -215,5 +220,58 @@ class WaitingCustomerServiceTest {
         assertThat(waitingRepository.findById(waiting.getId()).get().getWaitingStatus()).isEqualTo(
                 WaitingStatus.CUSTOMER_CANCELED);
         assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName(" 웨이팅 100개 등록")
+    void createWaitingByNotConcurrency() throws InterruptedException {
+        // given
+        Customer mockCustomer = Customer.builder()
+                .memberId(1L)
+                .memberPhone("010-1234-1234")
+                .build();
+        when(authService.getAuthenticatedMember()).thenReturn(mockCustomer);
+        // when
+        for (int i = 0; i < 100; i++) {
+            waitingCustomerService.createWaiting(1L, 2);
+        }
+
+        // then
+        Long findResult = waitingRepository.findMaxWaitingOrderByStoreId(1L);
+        assertThat(findResult).isEqualTo(100);
+    }
+
+    //    @Disabled
+    @Test
+    @Rollback(value = false)
+    @DisplayName("동시에 웨이팅 100개 등록 동시성 테스트")
+    void createWaitingByConcurrency() throws InterruptedException {
+        // given
+        int threadCount = 50;
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        Customer mockCustomer = Customer.builder()
+                .memberId(1L)
+                .memberPhone("010-1234-1234")
+                .build();
+        when(authService.getAuthenticatedMember()).thenReturn(mockCustomer);
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executorService.execute(() -> {
+                try {
+                    waitingCustomerService.createWaiting(1L, 2);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+        executorService.shutdown();
+
+        // then
+        Long findResult = waitingRepository.findMaxWaitingOrderByStoreId(1L);
+
+        assertThat(findResult).isEqualTo(50);
     }
 }
