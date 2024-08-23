5ce8da291a3838f4a132f5c29323997705c59154
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-23 17:48:50 +0900

    feat(WaitingCustomerService): apply Redission 분산 락 #308

--- src/main/java/com/matdang/seatdang/waiting/service/WaitingCustomerService.java
+++ src/main/java/com/matdang/seatdang/waiting/service/WaitingCustomerService.java
@@ -9,10 +9,15 @@ import com.matdang.seatdang.waiting.repository.WaitingRepository;
 import com.matdang.seatdang.waiting.repository.query.WaitingQueryRepository;
 import com.matdang.seatdang.waiting.repository.query.dto.WaitingInfoProjection;
 import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,9 +25,14 @@ public class WaitingCustomerService {
     private final WaitingRepository waitingRepository;
     private final WaitingQueryRepository waitingQueryRepository;
     private final AuthService authService;
+    private final RedissonClient redissonClient; // RedissonClient 추가
 
+    @Transactional
+    public Long createWaiting(Long storeId, Integer peopleCount) {
+        RLock lock = redissonClient.getLock("waitingLock:" + storeId); // 락 키 설정
+        lock.lock(); // 락 획득
 
-    public synchronized Long createWaiting(Long storeId, Integer peopleCount) {
+        try {
         Member customer = authService.getAuthenticatedMember();
 
         Waiting waiting = Waiting.builder()
@@ -39,14 +49,27 @@ public class WaitingCustomerService {
                 .build();
 
         return waitingRepository.save(waiting).getId();
+        } finally {
+            lock.unlock(); // 락 해제
+        }
     }
 
     @Transactional
     public int cancelWaitingByCustomer(Long waitingId) {
-        Waiting waiting = waitingRepository.findById(waitingId).get();
-        int result = waitingRepository.updateWaitingOrderByCancel(waiting.getStoreId(), waiting.getWaitingOrder());
+        Optional<Waiting> optionalWaiting = waitingRepository.findById(waitingId);
+        Waiting waiting = optionalWaiting.orElseThrow(() -> new NoSuchElementException("No waiting found with id: " + waitingId));
 
-        return waitingRepository.cancelWaitingByCustomer(waitingId) + result;
+//        Waiting waiting = waitingRepository.findById(waitingId).get();
+        RLock lock = redissonClient.getLock("waitingLock:" + waiting.getStoreId()); // 락 키 설정
+        lock.lock(); // 락 획득
+
+        try {
+            int result = waitingRepository.updateWaitingOrderByCancel(waiting.getStoreId(), waiting.getWaitingOrder());
+
+            return waitingRepository.cancelWaitingByCustomer(waitingId) + result;
+        } finally {
+            lock.unlock();
+        }
     }
 
 
