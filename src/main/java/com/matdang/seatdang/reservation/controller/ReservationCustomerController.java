778a304bf94283f0efd97588d8ed2f34720e74d4
CstoneKu <162235645+CstoneKu@users.noreply.github.com>
2024-08-26 18:11:58 +0900

    feature: 일반 예약 생성 및 결제 구현 완료 #36

--- src/main/java/com/matdang/seatdang/reservation/controller/ReservationCustomerController.java
+++ src/main/java/com/matdang/seatdang/reservation/controller/ReservationCustomerController.java
@@ -10,6 +10,7 @@ import com.matdang.seatdang.member.entity.Member;
 import com.matdang.seatdang.member.entity.MemberRole;
 import com.matdang.seatdang.member.service.CustomerService;
 import com.matdang.seatdang.member.service.StoreOwnerMemberService;
+import com.matdang.seatdang.payment.dto.PayDetail;
 import com.matdang.seatdang.reservation.dto.*;
 import com.matdang.seatdang.reservation.service.ReservationCommandService;
 import com.matdang.seatdang.reservation.service.ReservationQueryService;
@@ -26,6 +27,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -117,9 +119,48 @@ public class ReservationCustomerController {
     }
 
     @PostMapping("/create-normal")
-    public String test(@ModelAttribute ReservationSaveRequestDto saveRequestDto){
-        log.debug("saveRequest Test Dto: {}", saveRequestDto.toString());
-        return "redirect:/my-seat-dang/store/detail/6";
+    public String normal(@ModelAttribute("custom_reservation") ReservationSaveRequestDto saveDto
+                        , RedirectAttributes redirectAttributes
+                        , Model model){
+        log.debug("saveRequest Test Dto: {}", saveDto.toString());
+        StoreOwnerResponseDto storeOwner = storeOwnerMemberService.findByStoreId(saveDto.getStore().getStoreId());
+        saveDto.setStoreOwner(new StoreOwnerInfo(storeOwner.getStoreOwnerId(), storeOwner.getStoreOwnerName()));
+        Member member = authService.getAuthenticatedMember();
+        saveDto.setCustomer(new CustomerInfo(member.getMemberId(), member.getMemberName(), member.getMemberPhone()));
+        Long reservationId = null;
+
+        //상점에서 예약한도 가져오기
+        int maxReservationInTime = storeService.getMaxReservationInTime(saveDto.getStore().getStoreId());
+        ReservationTicketRequestDTO ticketRequestDTO = ReservationTicketRequestDTO.builder()
+                .storeId(saveDto.getStore().getStoreId())
+                .date(saveDto.getDate())
+                .time(saveDto.getTime())
+                .maxReservation(maxReservationInTime)
+                .build();
+        //예약 한도 조회
+        ReservationTicket ticket = reservationSlotCommandService.getReservationTicket(ticketRequestDTO);
+        //예약 가능 키를 획득했으면 예약
+
+        if(ticket.equals(ReservationTicket.AVAILABLE)){
+            //예약 티켓 획득 성공시 처리
+            reservationId = reservationCommandService.createNormalMenuReservation(saveDto);
+        }else{
+            //예약 티켓 획득 실패시 처리
+            throw new RuntimeException("이미 마감된 예약입니다");
+        }
+
+        PayDetail payDetail = PayDetail.builder()
+                .partnerOrderId(reservationId)   //reservationId
+                .partnerUserId(saveDto.getCustomer().getCustomerId())    //customerId
+                .shopId(saveDto.getStore().getStoreId())   //storeId
+                .itemName(saveDto.getMenuList().get(0).getMenuName())
+                .quantity(1)
+                .totalAmount(saveDto.getTotalAmount())
+                .taxFreeAmount(0)    //상품 비과세 금액
+                .build();
+        redirectAttributes.addFlashAttribute("payDetail" , payDetail);
+
+        return "redirect:/payment/request";
     }
 
     @PostMapping("/cancel")
