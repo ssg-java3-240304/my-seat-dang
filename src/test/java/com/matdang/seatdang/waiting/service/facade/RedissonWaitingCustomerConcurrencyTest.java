3c9e03c214cd15a047f61c802c3cb2515a5e9ddb
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-27 14:22:57 +0900

    test(waiting): modify Waiting Customer Concurrency test #308 #339

--- src/test/java/com/matdang/seatdang/waiting/service/facade/RedissonWaitingCustomerConcurrencyTest.java
+++ src/test/java/com/matdang/seatdang/waiting/service/facade/RedissonWaitingCustomerConcurrencyTest.java
@@ -5,8 +5,9 @@ import static org.mockito.Mockito.when;
 
 import com.matdang.seatdang.auth.service.AuthService;
 import com.matdang.seatdang.member.entity.Customer;
-import com.matdang.seatdang.waiting.repository.WaitingRepository;
-import com.matdang.seatdang.waiting.service.WaitingCustomerService;
+import com.matdang.seatdang.waiting.entity.Waiting;
+import com.matdang.seatdang.waiting.entity.WaitingStatus;
+import com.matdang.seatdang.waiting.service.WaitingService;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
@@ -20,21 +21,18 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
-class RedisWaitingCustomerConcurrencyTest {
+class RedissonWaitingCustomerConcurrencyTest {
     @Autowired
     private RedissonLockWaitingCustomerFacade redissonLockWaitingCustomerFacade;
-
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
     @Autowired
-    private WaitingRepository waitingRepository;
-    @Autowired
-    private WaitingCustomerService waitingCustomerService;
+    private WaitingService waitingService;
     @MockBean
     private AuthService authService;
 
     @Test
-    @DisplayName("동시에 웨이팅 100개 등록 Redis 동시성 테스트")
+    @DisplayName("동시에 웨이팅 100개 등록 동시성 테스트")
     void createWaitingByConcurrency() throws InterruptedException {
         // given
         int threadCount = 100;
@@ -50,7 +48,7 @@ class RedisWaitingCustomerConcurrencyTest {
         for (int i = 0; i < threadCount; i++) {
             executorService.execute(() -> {
                 try {
-                    waitingCustomerService.createWaiting(1L, 2);
+                    redissonLockWaitingCustomerFacade.createWaiting(1L, 2);
                 } finally {
                     latch.countDown();
                 }
@@ -72,7 +70,7 @@ class RedisWaitingCustomerConcurrencyTest {
     }
 
     @Test
-    @DisplayName("동시에 웨이팅 85개 취소 Redis 동시성 테스트")
+    @DisplayName("동시에 웨이팅 85개 취소 동시성 테스트")
     void cancelWaitingByConcurrency() throws InterruptedException {
         // given
         Customer mockCustomer = Customer.builder()
@@ -82,7 +80,7 @@ class RedisWaitingCustomerConcurrencyTest {
         when(authService.getAuthenticatedMember()).thenReturn(mockCustomer);
         List<Long> waitingIds = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            Long id = waitingCustomerService.createWaiting(1L, 2).getWaitingNumber();
+            Long id = redissonLockWaitingCustomerFacade.createWaiting(1L, 2).getWaitingNumber();
             waitingIds.add(id);
         }
 
@@ -95,7 +93,7 @@ class RedisWaitingCustomerConcurrencyTest {
             long waitingNumber = waitingIds.get(i);
             executorService.execute(() -> {
                 try {
-                    waitingCustomerService.cancelWaitingByCustomer(waitingNumber, 1L);
+                    redissonLockWaitingCustomerFacade.cancelWaitingByCustomer(waitingNumber, 1L);
                 } catch (Exception e) {
                     e.printStackTrace(); // 예외를 출력합니다
                 } finally {
@@ -107,14 +105,21 @@ class RedisWaitingCustomerConcurrencyTest {
         executorService.shutdown();
 
         // then
+        List<Waiting> waitings = redisTemplate.opsForHash().values("store:1").stream()
+                .map(waiting -> waitingService.convertStringToWaiting(waiting))
+                .filter(waiting -> waiting.getWaitingStatus() == WaitingStatus.CUSTOMER_CANCELED)
+                .toList();
+        assertThat(waitings.size()).isEqualTo(85);
+
         String max =(String) redisTemplate.opsForValue().get("waitingOrder:1");
         assertThat(Integer.parseInt(max)).isEqualTo(15);
+
 
         // 싱글 스레드 Redis 사용 - Redisson Lock 사용 X
         // 1577 ms, 1387ms, 1319ms
     }
     @Test
-    @DisplayName("웨이팅 100개 등록된 상태에서 등록 50번 + 취소50번 Redis 동시성 테스트")
+    @DisplayName("웨이팅 100개 등록된 상태에서 등록 50번 + 취소50번 동시성 테스트")
     void createAndCancelWaitingByConcurrency() throws InterruptedException {
         // given
         Customer mockCustomer = Customer.builder()
@@ -124,7 +129,7 @@ class RedisWaitingCustomerConcurrencyTest {
         when(authService.getAuthenticatedMember()).thenReturn(mockCustomer);
         List<Long> waitingIds = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            Long id = waitingCustomerService.createWaiting(1L, 2).getWaitingNumber();
+            Long id = redissonLockWaitingCustomerFacade.createWaiting(1L, 2).getWaitingNumber();
             waitingIds.add(id);
         }
 
@@ -137,7 +142,7 @@ class RedisWaitingCustomerConcurrencyTest {
             // 등록 50번
             executorService.execute(() -> {
                 try {
-                    waitingCustomerService.createWaiting(1L, 2);
+                    redissonLockWaitingCustomerFacade.createWaiting(1L, 2);
                 } catch (Exception e) {
                     e.printStackTrace(); // 예외를 출력합니다
                 } finally {
@@ -148,7 +153,7 @@ class RedisWaitingCustomerConcurrencyTest {
             long waitingNumber = waitingIds.get(i);
             executorService.execute(() -> {
                 try {
-                    waitingCustomerService.cancelWaitingByCustomer(waitingNumber, 1L);
+                    redissonLockWaitingCustomerFacade.cancelWaitingByCustomer(waitingNumber, 1L);
                 } catch (Exception e) {
                     e.printStackTrace(); // 예외를 출력합니다
                 } finally {
@@ -160,6 +165,23 @@ class RedisWaitingCustomerConcurrencyTest {
         executorService.shutdown();
 
         // then
+        List<Waiting> waitings = redisTemplate.opsForHash().values("store:1").stream()
+                .map(waiting -> waitingService.convertStringToWaiting(waiting))
+                .toList();
+        int waitingCount = 0;
+        int canceledCount = 0;
+        for (Waiting waiting : waitings) {
+            if (waiting.getWaitingStatus() == WaitingStatus.WAITING) {
+                waitingCount++;
+            }
+            if (waiting.getWaitingStatus() == WaitingStatus.CUSTOMER_CANCELED) {
+                canceledCount++;
+            }
+        }
+
+        assertThat(waitingCount).isEqualTo(100);
+        assertThat(canceledCount).isEqualTo(50);
+
         String max =(String) redisTemplate.opsForValue().get("waitingOrder:1");
         assertThat(Integer.parseInt(max)).isEqualTo(100);
 
