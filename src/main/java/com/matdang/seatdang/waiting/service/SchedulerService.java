ec75fc651d6eff74b7c166c1209da8ae5b286787
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-08-28 15:19:29 +0900

    fix(waiting): fix 웨이팅 마감 스케줄링 오류 #353

--- src/main/java/com/matdang/seatdang/waiting/service/SchedulerService.java
+++ src/main/java/com/matdang/seatdang/waiting/service/SchedulerService.java
@@ -2,21 +2,20 @@ package com.matdang.seatdang.waiting.service;
 
 import com.matdang.seatdang.store.entity.Store;
 import com.matdang.seatdang.store.repository.StoreRepository;
-import com.matdang.seatdang.store.repository.query.dto.StoreQueryRepository;
 import com.matdang.seatdang.store.vo.WaitingStatus;
-import com.matdang.seatdang.waiting.entity.Waiting;
 import com.matdang.seatdang.waiting.entity.WaitingStorage;
 import com.matdang.seatdang.waiting.repository.WaitingRepository;
 import com.matdang.seatdang.waiting.repository.WaitingStorageRepository;
 import com.matdang.seatdang.waiting.repository.query.WaitingQueryRepository;
-import java.time.Duration;
-import java.time.LocalTime;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.List;
 
 @Service
 @Transactional
@@ -54,27 +53,29 @@ public class SchedulerService {
     }
 
     // 10분 마다 실행
-    @Scheduled(cron = "0 0/10 * * * ?")
+    @Scheduled(cron = "0 0/18 * * * ?")
     public void closeWaiting() {
         log.info("[Close Waiting] This runs every 10 minutes");
-
+        LocalTime current = LocalTime.now();
         List<Store> stores = storeRepository.findAll();
         for (Store store : stores) {
-            if ((store.getStoreSetting().getWaitingStatus() == WaitingStatus.OPEN) &&
-                    (Duration.between(LocalTime.now(), store.getStoreSetting().getWaitingTime().getWaitingCloseTime())
-                            .toMinutes() <= 0)) {
+            LocalTime closeTime = store.getStoreSetting().getWaitingTime().getWaitingCloseTime();
 
-                int result = storeRepository.updateWaitingStatus(WaitingStatus.CLOSE, store.getStoreId());
-                if (result == 1) {
-                    log.info("=== Close Waiting ===");
-                    log.info("storeId ={}", store.getStoreId());
-                    log.info("=====================");
-                } else {
-                    log.error("=== Fail Close Waiting ===");
+            if ((closeTime.getHour() < 12 && current.getHour() < 12) || (current.getHour() >= 12 && closeTime.getHour() >= 12)) {
+                if ((store.getStoreSetting().getWaitingStatus() == WaitingStatus.OPEN) &&
+                        (Duration.between(current, closeTime)
+                                .toMinutes() <= 0)) {
+
+                    int result = storeRepository.updateWaitingStatus(WaitingStatus.CLOSE, store.getStoreId());
+                    if (result == 1) {
+                        log.info("=== Close Waiting ===");
+                        log.info("storeId ={}", store.getStoreId());
+                        log.info("=====================");
+                    } else {
+                        log.error("=== Fail Close Waiting ===");
+                    }
                 }
             }
-
         }
-
     }
 }
