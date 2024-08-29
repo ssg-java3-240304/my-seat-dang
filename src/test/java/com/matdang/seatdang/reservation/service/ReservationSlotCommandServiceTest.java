5f4776fa6152e2061d052be623001fd8d9210374
CstoneKu <162235645+CstoneKu@users.noreply.github.com>
2024-08-29 18:28:33 +0900

    feat: 이미 데이터 입력 되어 있는 경우 reservation slot 획득 테스트

--- src/test/java/com/matdang/seatdang/reservation/service/ReservationSlotCommandServiceTest.java
+++ src/test/java/com/matdang/seatdang/reservation/service/ReservationSlotCommandServiceTest.java
@@ -36,7 +36,53 @@ class ReservationSlotCommandServiceTest {
         LocalDate date = LocalDate.now();
         LocalTime time = LocalTime.of(12, 30);
 
-//        reservationSlotRepository.save(new ReservationSlot(storeId, date, time, maxReservation));
+        //동시 요청을 시뮬레이션을 하기 위한 Executor 준비
+        ExecutorService executor = Executors.newFixedThreadPool(numberOfRequests);
+        CountDownLatch latch = new CountDownLatch(numberOfRequests);
+
+        //요청을 만드는 callable 정의
+        Callable<ReservationTicket> requestTask = () ->{
+            try{
+                return reservationSlotCommandService.getReservationTicket(new ReservationTicketRequestDTO(storeId, date, time, maxReservation));
+            }finally {
+                latch.countDown();
+            }
+        };
+
+        //작업 제출
+        Future<ReservationTicket>[] futures = new Future[numberOfRequests];
+        for (int i = 0; i < numberOfRequests; i++) {
+            futures[i] = executor.submit(requestTask);
+        }
+
+        // 모든 작업이 세팅될때까지 대기
+        latch.await();
+
+        //when
+        int availableCnt = 0;
+        for(Future<ReservationTicket> future : futures){
+            if(future.get() == ReservationTicket.AVAILABLE){
+                availableCnt++;
+            }
+        }
+        //then
+        System.out.println(availableCnt);
+        assertThat(availableCnt).isEqualTo(maxReservation);
+        executor.shutdown();
+    }
+
+    @Transactional
+    @DisplayName("미리 입력된 데이터가 있을때, 예약제한 5개인 자원에 동시에 10개의 요청이 발생했을때 처리")
+    @Test
+    public void test1_2() throws Exception {
+        //given
+        int numberOfRequests = 100;
+        int maxReservation = 5;
+        Long storeId = 1L;
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.of(12, 30);
+
+        reservationSlotRepository.save(new ReservationSlot(storeId, date, time, maxReservation));
 
         //동시 요청을 시뮬레이션을 하기 위한 Executor 준비
         ExecutorService executor = Executors.newFixedThreadPool(numberOfRequests);
